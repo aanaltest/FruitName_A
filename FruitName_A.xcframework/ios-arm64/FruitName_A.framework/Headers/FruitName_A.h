0af6fc49d42a6e6f90e2ec72cee352d571b882d3
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-11 17:09:10 +0530

    added readme file and framework

---
+++ FruitName_A.xcframework/ios-arm64/FruitName_A.framework/Headers/FruitName_A.h
@@ -0,0 +1,18 @@
+//
+//  FruitName_A.h
+//  FruitName_A
+//
+//  Created by SAPPHIRE on 11/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FruitName_A.
+FOUNDATION_EXPORT double FruitName_AVersionNumber;
+
+//! Project version string for FruitName_A.
+FOUNDATION_EXPORT const unsigned char FruitName_AVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruitName_A/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# FruitName_A
